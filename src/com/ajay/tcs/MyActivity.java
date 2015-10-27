4ae9bbde556f082fc3e5c20363d3d2a7df27e2b8
ajay8704276 <adyrocks1989@hotmail.com>
2015-10-27 18:13:01 +0530

    Initial Commit - git - LILO InfiniteRunner check

--- src/com/ajay/tcs/MyActivity.java
+++ src/com/ajay/tcs/MyActivity.java
@@ -12,5 +12,7 @@ public class MyActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         System.out.print("ajay");
+        System.out.print("ajay");
+
     }
 }
